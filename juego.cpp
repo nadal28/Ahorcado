44703373f209dad315b10817d3f1b4c3d50ea75e
Adrian Aragones Jimenez <aaragones@dam1-pc00.(none)>
2015-06-02 14:16:07 +0200

    update

--- juego.cpp
+++ juego.cpp
@@ -1,19 +1,33 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#define N 30
 
-char palabra[30];
+char palabra[N];
 int tamano;
 int vidas = 5;
 char letra;
+char aciertos[N];
+int nAciertos;
 
-void desplegarTablero(){
+void registrarAcierto(char){
+    letra = aciertos[nAciertos];
+    nAciertos++;
+    if(nAciertos == tamano)
+	printf("Has ganado, la palabra era: ", palabra);
+
+}
+
+void desplegarTablero(char){
     tamano = strlen(palabra);
     while(vidas > 0){
 	printf("Introduce una letra: ");
 	gets(letra);
-	
 	for(int i=0; i < tamano; i++)
+	    if(palabra[i] == letra)
+		registrarAcierto(letra);	
+	    else
+
 
     }
 
@@ -26,7 +40,7 @@ int main(){
     printf("Escribe una palabra: ");
     gets(palabra);
 
-    desplegarTablero();
+    desplegarTablero(palabra);
 
 
 
