e1aabccc5a784d876c9c9f1d156dce639c2ea02c
Adrian Aragones Jimenez <aaragones@dam1-pc00.(none)>
2015-06-02 12:31:04 +0200

    update

--- juego.cpp
+++ juego.cpp
@@ -2,7 +2,16 @@
 #include <stdlib.h>
 #include <string.h>
 
+char palabra[30];
+
 int main(){
     system("clear");
     system("toilet -Fmetal Ahorcado");
+
+    printf("Escribe una palabra: ");
+    gets(palabra);
+
+    printf("%s",palabra);
+
+
 }
