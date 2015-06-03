0c4bde8bd17e853d5b6aef28030e225d7be97e69
Adrian Aragones Jimenez <aaragones@dam1-pc00.(none)>
2015-06-03 13:28:13 +0200

    update

--- main.cpp
+++ main.cpp
@@ -7,7 +7,7 @@ int main(){
     char letra;
     int vidas = 5;
     int correcto;
-    
+
     bool repite;
     system("clear");
     system("toilet -Fmetal Ahorcado by nadal28");
@@ -20,48 +20,48 @@ int main(){
 	printf(" _ ");
 
     while(vidas>0 ){	
-	    system("clear");
-	    system("toilet -Fmetal Ahorcado by nadal28");
-	    printf("\nVidas:  %i\n", vidas);		
-	    printf("Introduce una letra: ");
+	system("clear");
+	system("toilet -Fmetal Ahorcado by nadal28");
+	printf("\nVidas:  %i\n", vidas);		
+	printf("Introduce una letra: ");
 
-	    scanf(" %c", &letra);
-	    correcto = 0;
-	    repite = false;
-	    for(int j=0; j<strlen(palabra); j++){
+	scanf(" %c", &letra);
+	correcto = 0;
+	repite = false;
+	for(int j=0; j<strlen(palabra); j++){
 
 	    if(letra == acierto[j]){
-	    printf("Otra vez? Pues te quito una vida cabron\n");
-	    vidas--;
-	    repite = true;		
+		printf("Otra vez? Pues te quito una vida cabron\n");
+		vidas--;
+		repite = true;		
 	    }else if(letra == palabra[j]){
-	    acierto[j] = letra;
-	    
-	    correcto++;
+		acierto[j] = letra;
+
+		correcto++;
 	    }else if(correcto == 0 && j == (strlen(palabra)-1) && repite == false){
-	    vidas--;
-	    
+		vidas--;
 
-	    correcto++;
+
+		correcto++;
 	    }		
-	    }
-	    for(int a=0; a<strlen(palabra);a++){
-		if(acierto[a] == ' ')
-		    printf("_");
-		else{
-		    printf(" %c ",acierto[a]);
-				}
+	}
+	for(int a=0; a<strlen(palabra);a++){
+	    if(acierto[a] == ' '){
+		printf("_");
+	    } else{
+		printf(" %c ",acierto[a]);
+	   }
 
-	    }
-   if((strcmp(acierto,palabra)) == 0){
-   printf("\nHas ganado\n");
-   return 0;
-   }
+	}
+	if((strcmp(acierto,palabra)) == 0){
+	    printf("\nHas ganado\n");
+	    return 0;
+	}
 
 
     }
 
-   	printf("Has perdido, la palabra era %s", palabra);
+    printf("Has perdido, la palabra era %s", palabra);
     return 0;
 
 }
